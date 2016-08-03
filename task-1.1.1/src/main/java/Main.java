535b0ec94ba178d5f8ed7a9b2b8d90c85561fd1f
Pavel Volokitin <pvolok@gmail.com>
2016-08-03 18:48:14 +0300

    Завершена реализация задачи 1.1.1.

--- task-1.1.1/src/main/java/Main.java
+++ task-1.1.1/src/main/java/Main.java
@@ -4,20 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        int a = in.nextInt();
-        int b = in.nextInt();
+        System.out.print("Введите значение переменной a: ");
+        int a = scanner.nextInt();
+        System.out.print("Введите значение переменной b: ");
+        int b = scanner.nextInt();
 
         System.out.println("Переменная a: " + a);
         System.out.println("Переменная b: " + b);
-        System.out.println("Нажмите любую клавишу.");
 
-        System.console().readLine();
+        scanner.nextLine();
+        System.out.println("\n" + "Нажмите любую клавишу, чтобы поменять переменные местами.");
+        scanner.nextLine();
 
         int temp = a;
         a = b;
         b = temp;
+
+        System.out.println("Переменная a теперь равна: " + a);
+        System.out.println("Переменная b теперь равна: " + b);
 
     }
 
