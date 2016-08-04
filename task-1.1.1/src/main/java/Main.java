3f7d4f48a9404c8e651242b5800c9aec24c7f344
Pavel Volokitin <pvolok@gmail.com>
2016-08-04 09:48:11 +0300

    Решение задачи оформлено в виде функции. Добавлены комментарии.

--- task-1.1.1/src/main/java/Main.java
+++ task-1.1.1/src/main/java/Main.java
@@ -6,24 +6,51 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите значение переменной a: ");
-        int a = scanner.nextInt();
-        System.out.print("Введите значение переменной b: ");
-        int b = scanner.nextInt();
+        int[] numbers = new int[2];
 
-        System.out.println("Переменная a равна: " + a);
-        System.out.println("Переменная b равна: " + b);
+        System.out.print("Введите значение переменной a: ");
+        numbers[0] = scanner.nextInt();
+        System.out.print("Введите значение переменной b: ");
+        numbers[1] = scanner.nextInt();
+
+        System.out.println("\n" + "Переменная a равна: " + numbers[0]);
+        System.out.println("Переменная b равна: " + numbers[1]);
 
         System.out.println("\n" + "Меняю переменные местами..." + "\n");
+        numbers = variableChanger(numbers);
 
         Thread.sleep(1000);
 
-        int temp = a;
+        System.out.println("Переменная a теперь равна: " + numbers[0]);
+        System.out.println("Переменная b теперь равна: " + numbers[1]);
+
+    }
+
+    // Функция принимает массив целых чисел с двумя элементами (если это не так возвращает null) и меняет местами два
+    // элемента.
+    private static int[] variableChanger(int[] someArray) {
+
+        // Проверка на длину.
+        if (someArray.length != 2) {
+            return null;
+        }
+
+        // Доополнитльные пересенные введены для наглядности.
+        int a = someArray[0];
+        int b = someArray[1];
+        int temp;
+
+        // Действия из решения задачи.
+        temp = a;
         a = b;
         b = temp;
 
-        System.out.println("Переменная a теперь равна: " + a);
-        System.out.println("Переменная b теперь равна: " + b);
+        // Присвоение измененных значений соответсвующим ячейкам массива.
+        someArray[0] = a;
+        someArray[1] = b;
+
+        // Возврат результата.
+        return someArray;
 
     }
 
