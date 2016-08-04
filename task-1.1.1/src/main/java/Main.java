565c33add152e1fadd8615b80c091b8f2ccd590e
Pavel Volokitin <pvolok@gmail.com>
2016-08-04 10:13:54 +0300

    Добавлены комментарии главный метод.

--- task-1.1.1/src/main/java/Main.java
+++ task-1.1.1/src/main/java/Main.java
@@ -4,23 +4,30 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
+        // Создаем новый Scanner для получения ввода пользоватепля
         Scanner scanner = new Scanner(System.in);
 
+        // Создаем массив для значений введенных пользователем.
         int[] numbers = new int[2];
 
+        // Выводим инструкции и записываем в соответствующие ячейки массива введеннын пользователем значения.
         System.out.print("Введите значение переменной a: ");
         numbers[0] = scanner.nextInt();
         System.out.print("Введите значение переменной b: ");
         numbers[1] = scanner.nextInt();
 
+        // Выводим сообщения с результатами записи переменных в массив.
         System.out.println("\n" + "Переменная a равна: " + numbers[0]);
         System.out.println("Переменная b равна: " + numbers[1]);
 
+        // Выводим сообщение и запускаем функцию смены переменных местами.
         System.out.println("\n" + "Меняю переменные местами..." + "\n");
         numbers = variableChanger(numbers);
 
+        // Делаем паузу в одну секунду для имитации работы функции.
         Thread.sleep(1000);
 
+        // Выводим результат.
         System.out.println("Переменная a теперь равна: " + numbers[0]);
         System.out.println("Переменная b теперь равна: " + numbers[1]);
 
