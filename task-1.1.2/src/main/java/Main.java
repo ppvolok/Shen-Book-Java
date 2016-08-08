37e9d3dff5507e65a041cd878e7f30de268aca14
Pavel Volokitin <pvolok@gmail.com>
2016-08-08 16:27:29 +0300

    Задача 1.1.2 переделана для работы с аргументами командной строки. Тип переменной изменен на String. Актуализированы комментарии.

--- task-1.1.2/src/main/java/Main.java
+++ task-1.1.2/src/main/java/Main.java
@@ -4,32 +4,63 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        // Создаем новый Scanner для получения ввода пользователя.
-        Scanner scanner = new Scanner(System.in);
-
         // Создаем массив для значений введенных пользователем.
         int[] someNumbers = new int[2];
 
-        // Выводим инструкции и записываем в соответствующие ячейки массива введеннын пользователем значения.
-        System.out.print("Введите значение переменной a: ");
-        someNumbers[0] = scanner.nextInt();
-        System.out.print("Введите значение переменной b: ");
-        someNumbers[1] = scanner.nextInt();
+        if (args.length == 2) {
 
-        // Выводим сообщения с результатами записи переменных в массив.
-        System.out.println("\n" + "Переменная a равна: " + someNumbers[0]);
-        System.out.println("Переменная b равна: " + someNumbers[1]);
+            someNumbers[0] = Integer.parseInt(args[0]);
+            someNumbers[1] = Integer.parseInt(args[1]);
 
-        // Выводим сообщение и запускаем функцию смены переменных местами.
-        System.out.println("\n" + "Меняю переменные местами..." + "\n");
-        someNumbers = numberVariableChanger(someNumbers);
+            // Выводим сообщения с результатами записи переменных в массив.
+            System.out.println("Переменная a равна: " + someNumbers[0]);
+            System.out.println("Переменная b равна: " + someNumbers[1]);
 
-        // Делаем паузу в одну секунду для имитации работы функции.
-        Thread.sleep(1000);
+            // Выводим сообщение и запускаем функцию смены переменных местами.
+            System.out.println("\n" + "Меняю переменные местами..." + "\n");
+            someNumbers = numberVariableChanger(someNumbers);
 
-        // Выводим результат.
-        System.out.println("Переменная a теперь равна: " + someNumbers[0]);
-        System.out.println("Переменная b теперь равна: " + someNumbers[1]);
+            // Делаем паузу в одну секунду для имитации работы функции.
+            Thread.sleep(1000);
+
+            // Выводим результат.
+            System.out.println("Переменная a теперь равна: " + someNumbers[0]);
+            System.out.println("Переменная b теперь равна: " + someNumbers[1]);
+
+
+        } else if (args.length == 0) {
+
+            // Создаем новый Scanner для получения ввода пользователя.
+            Scanner scanner = new Scanner(System.in);
+
+            // Выводим инструкции и записываем в соответствующие ячейки массива введеннын пользователем значения.
+            System.out.print("Введите значение переменной a: ");
+            someNumbers[0] = scanner.nextInt();
+            System.out.print("Введите значение переменной b: ");
+            someNumbers[1] = scanner.nextInt();
+
+            // Выводим сообщения с результатами записи переменных в массив.
+            System.out.println("\n" + "Переменная a равна: " + someNumbers[0]);
+            System.out.println("Переменная b равна: " + someNumbers[1]);
+
+            // Выводим сообщение и запускаем функцию смены переменных местами.
+            System.out.println("\n" + "Меняю переменные местами..." + "\n");
+            someNumbers = numberVariableChanger(someNumbers);
+
+            // Делаем паузу в одну секунду для имитации работы функции.
+            Thread.sleep(1000);
+
+            // Выводим результат.
+            System.out.println("Переменная a теперь равна: " + someNumbers[0]);
+            System.out.println("Переменная b теперь равна: " + someNumbers[1]);
+
+        } else {
+
+            // Для всех иных вариантов указания аргументов, выводим справку.
+            System.out.println("Аргументы указаны неверно!");
+            System.out.println("Правильный формат: \"имя_приложения целое_число_1 целое_число_2\"");
+
+        }
 
     }
 
