549d10b280fdabf2d56ee90f51cbe488d1828dce
Pavel Volokitin <pvolok@gmail.com>
2016-08-08 16:04:44 +0300

    Задача 1.1.1 переделана для работы с аргументами командной строки. Тип переменной изменен на String. Актуализированы комментарии.

--- task-1.1.1/src/main/java/Main.java
+++ task-1.1.1/src/main/java/Main.java
@@ -4,42 +4,75 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        // Создаем новый Scanner для получения ввода пользователя.
-        Scanner scanner = new Scanner(System.in);
-
         // Создаем массив для значений введенных пользователем.
-        int[] numbers = new int[2];
+        String[] strings = new String[2];
 
-        // Выводим инструкции и записываем в соответствующие ячейки массива введеннын пользователем значения.
-        System.out.print("Введите значение переменной a: ");
-        numbers[0] = scanner.nextInt();
-        System.out.print("Введите значение переменной b: ");
-        numbers[1] = scanner.nextInt();
+        // Если аргументов два, то...
+        if (args.length == 2) {
 
-        // Выводим сообщения с результатами записи переменных в массив.
-        System.out.println("\n" + "Переменная a равна: " + numbers[0]);
-        System.out.println("Переменная b равна: " + numbers[1]);
+            // ...присваиваем их соответсвующим ячейкам массива.
+            strings[0] = args[0];
+            strings[1] = args[1];
 
-        // Выводим сообщение и запускаем функцию смены переменных местами.
-        System.out.println("\n" + "Меняю переменные местами..." + "\n");
-        numbers = variableChanger(numbers);
+            // Выводим сообщения с результатами записи переменных в массив.
+            System.out.println("\n" + "Переменная a равна: " + strings[0]);
+            System.out.println("Переменная b равна: " + strings[1]);
 
-        // Делаем паузу в одну секунду для имитации работы функции.
-        Thread.sleep(1000);
+            // Выводим сообщение и запускаем функцию смены переменных местами.
+            System.out.println("\n" + "Меняю переменные местами..." + "\n");
+            strings = variableChanger(strings);
 
-        // Выводим результат.
-        System.out.println("Переменная a теперь равна: " + numbers[0]);
-        System.out.println("Переменная b теперь равна: " + numbers[1]);
+            // Делаем паузу в одну секунду для имитации работы функции.
+            Thread.sleep(1000);
+
+            // Выводим результат.
+            System.out.println("Переменная a теперь равна: " + strings[0]);
+            System.out.println("Переменная b теперь равна: " + strings[1]);
+
+        } else if (args.length == 0) {
+
+            // Создаем новый Scanner для получения ввода пользователя.
+            Scanner scanner = new Scanner(System.in);
+
+
+            // Выводим инструкции и записываем в соответствующие ячейки массива введеннын пользователем значения.
+            System.out.print("Введите значение переменной a: ");
+            strings[0] = scanner.nextLine();
+            System.out.print("Введите значение переменной b: ");
+            strings[1] = scanner.nextLine();
+
+            // Выводим сообщения с результатами записи переменных в массив.
+            System.out.println("\n" + "Переменная a равна: " + strings[0]);
+            System.out.println("Переменная b равна: " + strings[1]);
+
+            // Выводим сообщение и запускаем функцию смены переменных местами.
+            System.out.println("\n" + "Меняю переменные местами..." + "\n");
+            strings = variableChanger(strings);
+
+            // Делаем паузу в одну секунду для имитации работы функции.
+            Thread.sleep(1000);
+
+            // Выводим результат.
+            System.out.println("Переменная a теперь равна: " + strings[0]);
+            System.out.println("Переменная b теперь равна: " + strings[1]);
+
+        } else {
+
+            // Для всех иных вариантов указания аргументов, выводим справку.
+            System.out.println("Аргументы указаны неверно!");
+            System.out.println("Правильный формат: \"имя_приложения переменная_1 переменная_2\"");
+
+        }
 
     }
 
     // Функция принимает массив целых чисел и меняет местами первые два элемента.
-    private static int[] variableChanger(int[] someArray) {
+    private static String[] variableChanger(String[] someArray) {
 
         // Дополнительные переменные введены для наглядности.
-        int a = someArray[0];
-        int b = someArray[1];
-        int temp;
+        String a = someArray[0];
+        String b = someArray[1];
+        String temp;
 
         // Действия из решения задачи.
         temp = a;
